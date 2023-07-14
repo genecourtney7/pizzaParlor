d3ad170ceafebac8a8019503fda529b2fc6d5164
gene courtney <genecourtney7@gmail.com>
2023-07-13 22:14:30 -0500

    I should have added an object of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-//businessLogic
+//First create pizza object.
 
-//userInterfaceLogic
+//Then create separate toppings (pepperoni, sausage, canadianBacon, peppers, mushrooms).
+
+//Create functions to place toppings on pizza.
+
+//Present pizza with all the toppings on it.
+
+//Place order.
+
+//Create new order.
+
+
+//Business Logic
+let pizza = new object()
+    pizza.size = "large";
+    pizza.toppings = ["Cheese"];
+
+
+//UI Logic
